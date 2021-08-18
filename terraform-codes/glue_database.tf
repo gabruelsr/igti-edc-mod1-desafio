596f213e7105defcc32656b518d5196f780bf2e2
gabruelsr <86360136+gabruelsr@users.noreply.github.com>
2021-08-18 20:55:55 -0300

    ajustando localização da database

--- terraform-codes/glue_database.tf
+++ terraform-codes/glue_database.tf
@@ -2,5 +2,5 @@
 resource "aws_glue_catalog_database" "catalog-desafio" {
   name = "igti-edc-mod1-desafio"
   description = "catalogo de raw data do desafio igti edc mod 1"
-  location_uri = "s3://${aws_s3_bucket.desafio.id}/raw-zone/"
+  location_uri = "s3://${aws_s3_bucket.desafio.id}/staging-zone/"
 }
