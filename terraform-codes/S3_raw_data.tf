5aa1831db1ffad6687540aaaa15002905ff5bedf
gabruelsr <86360136+gabruelsr@users.noreply.github.com>
2021-08-18 21:09:46 -0300

    correção no upload de arquivos

--- terraform-codes/S3_raw_data.tf
+++ terraform-codes/S3_raw_data.tf
@@ -2,6 +2,6 @@
 resource "aws_s3_bucket_object" "raw_data" {
   for_each = fileset("../microdados_educacao_basica_2020", "*")
   bucket = aws_s3_bucket.desafio.id
-  key    = "gabruelsr-igti-desafio-mod1/raw_zone/raw_data/${each.key}"
+  key    = "gabruelsr-igti-desafio-mod1/raw-zone/raw-data/${each.key}"
   source = "../microdados_educacao_basica_2020/${each.key}"
 }
