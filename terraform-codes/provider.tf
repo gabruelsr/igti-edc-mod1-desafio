5563f2b38ec17578ff78197d55f9c4fc0c244f6f
gabruelsr <86360136+gabruelsr@users.noreply.github.com>
2021-08-19 13:19:11 -0300

    estado do terraform

--- terraform-codes/provider.tf
+++ terraform-codes/provider.tf
@@ -4,3 +4,12 @@ provider "aws" {
   # Parâmetros
   region = var.region
 }
+
+# Centralizar o arquivo de controle de estado do terraform
+terraform {
+  backend "s3" {
+    bucket = "gabruelsr-igti-desafio-mod1"
+    key    = "state/terraform/terraform.tfstate"
+    region = "us-east-2"
+  }
+} 
