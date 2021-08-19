dbb8c2a172c27d12f7b0ab850204098ab0ba0aed
gabruelsr <86360136+gabruelsr@users.noreply.github.com>
2021-08-19 15:11:46 -0300

    move script upload to flue job code

--- terraform-codes/S3_raw_data.tf
+++ terraform-codes/S3_raw_data.tf
@@ -5,11 +5,3 @@ resource "aws_s3_bucket_object" "raw_data" {
   key      = "raw-zone/raw-data/${each.key}"
   source   = "../microdados_educacao_basica_2020/${each.key}"
 }
-
-resource "aws_s3_bucket_object" "glue_job_script" {
-  bucket = aws_s3_bucket.desafio.bucket
-  key    = "support-zone/glue-scripts/glue_job_csv_to_parquet.py"
-  acl    = "private"
-  source = "../scripts/glue_job_csv_to_parquet.py"
-  etag   = filemd5("../scripts/glue_job_csv_to_parquet.py")
-}
--- terraform-codes/glue_to_parquet.tf
+++ terraform-codes/glue_to_parquet.tf
@@ -6,3 +6,11 @@ resource "aws_glue_job" "convert" {
     script_location = "s3://${aws_s3_bucket.desafio.bucket}/support-zone/glue-scripts/glue_job_csv_to_parquet.py"
   }
 }
+
+resource "aws_s3_bucket_object" "glue_job_script" {
+  bucket = aws_s3_bucket.desafio.bucket
+  key    = "support-zone/glue-scripts/glue_job_csv_to_parquet.py"
+  acl    = "private"
+  source = "../scripts/glue_job_csv_to_parquet.py"
+  etag   = filemd5("../scripts/glue_job_csv_to_parquet.py")
+}
