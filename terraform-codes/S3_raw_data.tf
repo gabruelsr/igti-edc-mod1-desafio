2c498359432d7b55f28e263d67048ae1c2fb0ed6
gabruelsr <86360136+gabruelsr@users.noreply.github.com>
2021-08-19 16:51:35 -0300

    fixing minor issues in tf codes

--- terraform-codes/S3_raw_data.tf
+++ terraform-codes/S3_raw_data.tf
@@ -2,6 +2,8 @@
 resource "aws_s3_bucket_object" "raw_data" {
   for_each = fileset("../microdados_educacao_basica_2020", "*")
   bucket   = aws_s3_bucket.desafio.bucket
+  acl = "private"
   key      = "raw-zone/raw-data/${each.key}"
   source   = "../microdados_educacao_basica_2020/${each.key}"
+  etag = filemd5("../microdados_educacao_basica_2020/${each.key}")
 }
